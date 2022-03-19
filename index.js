76e958a59009074753f52e1c1c2fb6514dd0ddb1
ZengJia <86162890+jiazengp@users.noreply.github.com>
2022-03-19 16:01:58 +0800

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+const exec = require('child_process').exec;
+const cmdStr = 'npm run start';
+exec(cmdStr, (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        console.warn(new Date(),' Bot 启动失败');
+    } else {
+        console.log(stdout);
+        console.warn(new Date(),' Bot 启动成功');
+    }
+});

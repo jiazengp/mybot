8c599490fe018739534930ab7e8a731ab1e0eb6f
ZengJia <86162890+jiazengp@users.noreply.github.com>
2022-03-19 16:11:07 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const exec = require('child_process').exec;
-const cmdStr = 'npm run start';
-exec(cmdStr, (err, stdout, stderr) => {
-    if (err){
-        console.log(err);
-        console.warn(new Date(),' Bot 启动失败');
-    } else {
-        console.log(stdout);
-        console.warn(new Date(),' Bot 启动成功');
-    }
-});
+module.exports = (req, res) => {
+    const exec = require('child_process').exec;
+    const cmdStr = 'npm run start';
+    exec(cmdStr, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            console.warn(new Date(), ' Bot 启动失败');
+        } else {
+            console.log(stdout);
+            console.warn(new Date(), ' Bot 启动成功');
+        }
+    });
+};
